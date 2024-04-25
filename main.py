85fe34b93a534c8ea00fc9cb41e0d45341896cef
aritrasa45 <143035320+aritrasa45@users.noreply.github.com>
2024-04-26 02:27:18 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,68 @@
+import time 
+#made by aritra
+#0aritrasa1
+
+
+print("""──▄▀▀▀▄▄▄▄▄▄▄▀▀▀▄───
+───█▒▒░░░░░░░░░▒▒█───
+───█░░AS░░░░░AS░░█────
+─▄▄──█░░░▀█▀░░░█──▄▄─
+█░░█─▀▄░░░░░░░▄▀─█░░█
+█▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█
+█░░╦─╦╔╗╦─╔╗╔╗╔╦╗╔╗░░█
+█░░║║║╠─║─║─║║║║║╠─░░█
+█░░╚╩╝╚╝╚╝╚╝╚╝╩─╩╚╝░░█
+█▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█
+
+
+
+
+
+""")
+
+time.sleep(1)
+print("welcome to number generrator \n V1")
+time.sleep(0.5)
+
+
+
+
+x=int(input("do u want to continue? \n{press 0 for [n]} \n{press 2 to [y]} \n   》"))
+
+
+if(x==2):
+    print("ok")
+
+else:
+    exit(1)
+    
+time.sleep(0.5)
+
+
+num=int(input("enter from which you want to start number  : "))
+time.sleep
+print("ok")
+
+
+a=int(input("input how many numbers \n you want to print   : "))
+
+print("ok")
+
+
+
+
+time.sleep(1.5)
+
+
+
+
+
+while(num<a):
+    print(num)
+    num= num + 1
+    
+    
+    
+        
+            
+    
---
+++ readme.md
@@ -0,0 +1,35 @@
+
+
+A small tool made by aritra... it can make numbers from 0 to infinite..
+
+
+for now im a small developer but the time goes i will goin to upload 
+
+
+more intresting and powerful tools with hardware support and hacking
+related...
+
+
+
+im sorry if anyone had faced any glitch or errors while using this..
+complain at:-
+
+INSTAGRAM-        0aritrasa1
+
+TELEGRAM-          zsxxsz1
+
+
+
+
+                       THANK YOU FOR USING THIS TOOL 
+
+                             MORE TOOLS COMING SOON 
+
+                                               
+
+
+
+
+
+
+                                                 ARITRA
