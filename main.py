fedcdad4b7d3a72e6bd279437af694361c00be6a
A.srkr <143035320+aritrasa45@users.noreply.github.com>
2024-05-05 11:56:54 +0530

    main.py

--- main.py
+++ main.py
@@ -1,56 +1,56 @@
 import time 
 #made by aritra
-#0aritrasa1
+#this code is proted by AGPL-3.0 license...not to repost anywhere
 
-
-print("""──▄▀▀▀▄▄▄▄▄▄▄▀▀▀▄───
-───█▒▒░░░░░░░░░▒▒█───
-───█░░AS░░░░░AS░░█────
-─▄▄──█░░░▀█▀░░░█──▄▄─
-█░░█─▀▄░░░░░░░▄▀─█░░█
-█▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█
-█░░╦─╦╔╗╦─╔╗╔╗╔╦╗╔╗░░█
-█░░║║║╠─║─║─║║║║║╠─░░█
-█░░╚╩╝╚╝╚╝╚╝╚╝╩─╩╚╝░░█
-█▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█
-
-
-
-
-
+print("""
+________  ________      
+|\   __  \|\   ____\     
+\ \  \|\  \ \  \___|_    
+ \ \   __  \ \_____  \               
+  \ \  \ \  \|____|\  \  
+   \ \__\ \__\____\_\  \ 
+    \|__|\|__|\_________\
+             \|_________|
+                         
 """)
 
 time.sleep(1)
-print("welcome to number generrator \n V1")
+print("welcome to number generrator \n V2")
 time.sleep(0.5)
 
-
-
-
-x=int(input("do u want to continue? \n{press 0 for [n]} \n{press 2 to [y]} \n   》"))
-
-
-if(x==2):
-    print("ok")
-
-else:
-    exit(1)
-    
+x=input("do u want to continue? \n [y/n]")
+if(x!='y' ):
+	print("wrong input")
+	print("closing....")
+	time.sleep(0.2)
+	exit(1)   
+	 
 time.sleep(0.5)
-
 
 num=int(input("enter from which you want to start number  : "))
 time.sleep
 print("ok")
 
-
 a=int(input("input how many numbers \n you want to print   : "))
 
 print("ok")
 
-
-
-
+if(a>10000):
+	z=input("Do u need 0.01 sec delays? [y/n]")
+	if(z=='y'):
+	   print("default is 0.01 sec, u can change the code ")
+	   time.sleep(1)
+	   print("starting in 5 sec")
+	   time.sleep(5)
+	   
+	   while(num<a):
+	   	print(num)
+	   	time.sleep(0.02) # here you can change the speed 
+	   	num= num + 1
+	   	time.sleep(0.001)
+		
+else:
+	pass
 time.sleep(1.5)
 
 
@@ -61,8 +61,5 @@ while(num<a):
     print(num)
     num= num + 1
     
-    
-    
-        
-            
+print("Thank you")    
     
