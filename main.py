f33bfe2aafbcdd5c71655f80c47fa25d91e98c89
A.srkr <143035320+aritrasa45@users.noreply.github.com>
2024-05-17 17:37:51 +0530

    main.py

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
-import time 
-#made by aritra
-#this code is proted by AGPL-3.0 license...not to repost anywhere
+mport time
+from colorama import Fore,Style, Back, init
 
-print("""
+init()
+def input_close(x):
+	if(x!='y'):
+		print("wrong input")
+		time.sleep(0.2)
+		print("closing...")
+		time.sleep(0.2)
+		exit(1)
+	else:
+		pass	
+
+def print_opreation(num,a):
+    while(num<a):
+    	print(num)
+    	num= num + 1	
+    		
+time.sleep(0.3)
+print(Fore.GREEN + """ 
 ________  ________      
 |\   __  \|\   ____\     
 \ \  \|\  \ \  \___|_    
@@ -13,53 +29,29 @@ ________  ________
              
                          
 """)
-
-time.sleep(1)
-print("welcome to number generrator \n V2")
+print(Style.RESET_ALL)	
 time.sleep(0.5)
+		
+print(Style.BRIGHT + "welcome to number generrator \n V2")
+print(Style.RESET_ALL)
+time.sleep(0.3)
 
-x=input("do u want to continue? \n [y/n]")
-if(x!='y' ):
-	print("wrong input")
-	print("closing....")
-	time.sleep(0.2)
-	exit(1)   
-	 
+x=input(Fore.RED + "do u want to continue? \n [y/n]")
+print(Style.RESET_ALL)  
+input_close(x)
 time.sleep(0.5)
 
 num=int(input("enter from which you want to start number  : "))
-time.sleep
+time.sleep(0.2)
 print("ok")
+time.sleep(0.5)
 
 a=int(input("input how many numbers \n you want to print   : "))
+time.sleep(0.5)
 
-print("ok")
+z= input("Do you want to countinue!?")
+time.sleep(0.5)
 
-if(a>10000):
-	z=input("Do u need 0.01 sec delays? [y/n]")
-	if(z=='y'):
-	   print("default is 0.01 sec, u can change the code ")
-	   time.sleep(1)
-	   print("starting in 5 sec")
-	   time.sleep(5)
-	   
-	   while(num<a):
-	   	print(num)
-	   	time.sleep(0.02) # here you can change the speed 
-	   	num= num + 1
-	   	time.sleep(0.001)
-		
-else:
-	pass
-time.sleep(1.5)
-
-
-
-
-
-while(num<a):
-    print(num)
-    num= num + 1
-    
-print("Thank you")    
-    
+input_close(z)
+print_opreation(num,a)
+  							
